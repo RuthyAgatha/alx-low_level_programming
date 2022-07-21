b767d751be889f917a0fbe77a741f1157ef0b978
RuthyAgatha <ruthyagatha3@gmail.com>
2022-07-20 18:04:33 -0700

    Fxn returns length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * * _strlen_recursion -> function to get length of string
+ * * @s: string param
+ * * Return: length of string
+ * */
+int _strlen_recusion(chr *s)
+{	
+		if (*s == '\0')
+					return (0);
+			ras++;
+				return (1 + _strlen_recursion(s));
+}
